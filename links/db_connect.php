6aa16264d9a9a09f6814ad4135d4bd7069a9ec9a
Alebarda410 <donat410@gmail.com>
2020-03-05 11:05:13 +0400

    final reg

--- links/db_connect.php
+++ links/db_connect.php
@@ -1,9 +1,9 @@
 <?php 
-if(file_exists('libs/rb.php')){
-    require 'libs/rb.php';
-}
-else{
-    require '../libs/rb.php';
-}
+    if(file_exists('libs/rb.php')){
+        require 'libs/rb.php';
+    }
+    else{
+        require '../libs/rb.php';
+    }
 R::setup( 'mysql:host=localhost;dbname=truprogram', 'root', 'usbw' );
 session_start();
--- links/signup.php
+++ links/signup.php
@@ -1,15 +1,19 @@
 <?php
 require "db_connect.php";
+$fname_l = strlen($_POST['full_name']);
+$email_l = strlen($_POST['email']);
+$pass_l = strlen($_POST['password']);
 if (R::count('users', "email = ?", [$_POST['email']]) > 0) {
     echo 'Такой Email уже зарегистроирован!';
-}
-elseif  ($_POST['password_2'] !== $_POST['password']) {
-    echo 'Вклюси js дебик!';
-}
-elseif(!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)){
-    echo 'Вклюси js дебик!';
-}
-else {
+} elseif ($fname_l == 0 || $fname_l > 100 || $email_l > 50) {
+    echo 'Юзай форму с JS падла!';
+} elseif ($_POST['password_2'] != $_POST['password'] && $pass_l> ) {
+    echo 'Юзай форму с JS падла!';
+} elseif (!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+    echo 'Юзай форму с JS падла!';
+} elseif (empty($_POST['rol']){
+    echo 'Юзай форму с JS падла!';
+} else {
     $user = R::dispense('users');
     $user->full_name = $_POST['full_name'];
     $user->email = $_POST['email'];

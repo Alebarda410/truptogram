8ae37fead41e14bd617f6dc3fd51c0acb8d3d1df
Alebarda410 <donat410@gmail.com>
2020-03-05 11:35:27 +0400

    fix fix reg

--- links/signup.php
+++ links/signup.php
@@ -2,16 +2,15 @@
 require "db_connect.php";
 $fname_l = strlen($_POST['full_name']);
 $email_l = strlen($_POST['email']);
-$pass_l = strlen($_POST['password']);
 if (R::count('users', "email = ?", [$_POST['email']]) > 0) {
     echo 'Такой Email уже зарегистроирован!';
 } elseif ($fname_l == 0 || $fname_l > 100 || $email_l > 50) {
     echo 'Юзай форму с JS падла!';
-} elseif ($_POST['password_2'] != $_POST['password'] && $pass_l> ) {
+} elseif ($_POST['password_2'] != $_POST['password']) {
     echo 'Юзай форму с JS падла!';
 } elseif (!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
     echo 'Юзай форму с JS падла!';
-} elseif (empty($_POST['rol']){
+} elseif (empty($_POST['rol'])){
     echo 'Юзай форму с JS падла!';
 } else {
     $user = R::dispense('users');

b75bfbcba5e9178843434a24017bd3646e19ebbd
Alebarda410 <53137759+Alebarda410@users.noreply.github.com>
2020-05-13 15:34:16 +0400

    добавил ловушку

---
+++ admin.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="ru">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1><?php echo $_SERVER["REMOTE_ADDR"] ?></h1>
+    <audio loop controls autoplay src="upload\272.wav"></audio>
+</body>
+
+</html>
